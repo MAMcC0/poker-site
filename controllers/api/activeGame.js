7ad4cb720cee88479063a081d68680a255cc0009
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-07-11 19:12:41 -0700

    created active game model

---
+++ controllers/api/activeGame.js
@@ -0,0 +1,6 @@
+import { solve, winners } from 'pokersolver';
+import express from "express";
+const router = express.Router();
+import  User  from '../../models/User.js';
+
+router.get()
--- public/js/threecardGame.js
+++ public/js/threecardGame.js
@@ -1,14 +1,20 @@
-import { solve, winners } from 'pokersolver';
-import { rank, wallet } from '../../models/User.js';
+import router from "../../controllers";
+
 const cardContainer = document.querySelector('.card-container');
 
 let anteAmount = 0;
 let playerAnte;
 
-
-
+//start game button calls fetch for GET route for active game
+// PUT wallet amount in active (foriegn key)
+//call for GET Route Card = randomize, sanatize with usable names for cards => front
+// GET Route for User 
+//needs to go to user routes GET request
+// onclick of ok put anteAmout into Active Game Route
 function initGame() {
     //randomize card for dealer and player and call ante function
+
+    router.get('/activeGame')
     function ante() {
         switch (rank) {
             case "C-Game":
@@ -34,6 +40,7 @@ function initGame() {
                 break;
 
         }
+        //check in backend
         //function to get more money
         if (wallet < anteAmount) {
             losersCorner();
@@ -45,6 +52,8 @@ function initGame() {
             //figure out how to match ante function
             //store playerAnteMatch in var
         }
+                //fetch request POST to activeGame
+               
 
         async function showCards() {
             await playerAnteMatch
@@ -54,7 +63,11 @@ function initGame() {
             playerAction()
         };
     }
-
+//template literal fetch for either bet or fold POST
+//button after bet to go to showdown
+//pass in hands into card route for solve hands, winner, string 
+//OK button for message for win/loss that triggers fetch POST request for wallet 
+// start round button appeaers
     function playerAction(event) {
         switch (event.target) {
             case "Bet":
