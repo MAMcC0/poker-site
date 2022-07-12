d5f271d27f88b3fdc7da2aacbd1757de893ed61c
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-07-11 20:41:53 -0700

    added post route for bets in activeGame routes

--- controllers/api/activeGame.js
+++ controllers/api/activeGame.js
@@ -1,6 +1,26 @@
 import { solve, winners } from 'pokersolver';
 import express from "express";
 const router = express.Router();
-import  User  from '../../models/User.js';
+// import  User  from '../../models/User.js';
+import  ActiveGame  from '../../models/activeGame.js';
+
 
 router.get()
+
+//Question: is it :bet for this post route?
+
+router.post('/:bet', async (req, res) => {
+    try {
+      const activeGameData = await ActiveGame.create(req.body);
+  
+      req.session.save(() => {
+        req.session.activeGame_bet = activeGameData.bet;
+  
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  
