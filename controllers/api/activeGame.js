ef3c646d17d1a966b4b85157959606a9495d3345
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-07-12 11:12:06 -0700

    added backend functionality for adding bets to activegame table

--- controllers/api/activeGame.js
+++ controllers/api/activeGame.js
@@ -9,18 +9,44 @@ router.get()
 
 //Question: is it :bet for this post route?
 
-router.post('/:bet', async (req, res) => {
+router.put('/:total_bet', async (req, res) => {
     try {
-      const activeGameData = await ActiveGame.create(req.body);
-  
-      req.session.save(() => {
-        req.session.activeGame_bet = activeGameData.bet;
-  
-        res.status(200).json(activeGameData);
-      });
+      const activeGameBets = await ActiveGame.create(totalBetAdd());
+        res.status(200).json(activeGameBets);
+      ;
     } catch (err) {
       res.status(400).json(err);
     }
   });
+// make sure to grab the input from front end in an array in an object ("type", "amount")
+  const totalBetAdd = (userInput) => {
 
-  
+    let totalBet;
+    switch (userInput.type){
+      case "ante":
+        ante(userInput.amount);
+        break;
+      case "pair-plus":
+          pairPlus(userInput.amount);
+          break;
+      case "bet":
+          bet(userInput.amount);
+          break;
+    };
+
+    const ante = (amount) => {
+      totalBet += amount;
+      return totalBet;
+    }
+
+    const pairPlus = (amount) => {
+      totalBet += amount;
+      return totalBet;
+    }
+
+    const bet = (amount) => {
+      totalBet += amount;
+      return totalBet;
+    }
+     return totalBet;
+  }
