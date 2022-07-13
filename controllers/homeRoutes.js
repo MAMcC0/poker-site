1061128e104ac8f2075f660bdcb09858fd488382
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-07-13 10:41:38 -0700

    server fix

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,22 +3,22 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    // try {
-    //   const dbGameLanding = await User.findAll({
-    //         attributes: ['money', 'username'],
-    //   });
+     try {
+       const dbGameLanding = await User.findAll({
+             attributes: ['wallet', 'user_name'],
+       });
   
-    //   const players = dbGameLanding.map((player) =>
-    //     player.get({ plain: true })
-    //   );
-    //   res.render('homepage', {
-    //     players,
-    //     loggedIn: req.session.loggedIn,
-    //   });
-    // } catch (err) {
-    //   console.log(err);
-    //   res.status(500).json(err);
-  //  }
+       const players = dbGameLanding.map((player) =>
+         player.get({ plain: true })
+       );
+       res.render('homepage', {
+         players,
+         loggedIn: req.session.loggedIn,
+       });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
 })
 
 export default router;
