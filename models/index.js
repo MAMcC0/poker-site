23106e7b5bdc023d49f793b82288be58236203ac
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-11-15 19:16:00 -0800

    push

--- models/index.js
+++ models/index.js
@@ -1,13 +1,8 @@
 import User from './User.js';
 import ActiveGame from './activeGame.js';
-import UserStats from './userstats.js';
-
 
 
 ActiveGame.hasOne(User, {
     foriegnKey: 'user_id',
 });
 
-UserStats.hasMany(User, {
-    foreignKey: 'user_id',
-})
